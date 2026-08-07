b5e3e46a61b3272766eefd2844a0f976ecf7c04d
Anand Kumar <anandkumargupta.akg15@gmail.com>
2026-08-07 23:51:51 +0530

    file & structure ready

---
+++ Automation-Exercise-POM-Framework/src/main/java/test.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class test {
+    public static void main(String[] args){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://automationexercise.com/");
+    }
+}
