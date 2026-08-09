3db425492abc88c5d38d64be51425f92ab431a36
Anand Kumar <anandkumargupta.akg15@gmail.com>
2026-08-09 18:53:26 +0530

    locators and methods created and its working fine

--- Automation-Exercise-POM-Framework/src/main/java/pages/HomePage.java
+++ Automation-Exercise-POM-Framework/src/main/java/pages/HomePage.java
@@ -17,10 +17,38 @@ public class HomePage {
 
     // Locator
     private By homeLink = By.linkText("Home");
+    private By products = By.cssSelector("a[href='/products']");
+    private By cart = By.cssSelector("a[href='/view_cart']");
+    private By login = By.cssSelector("a[href='/login']");
+    private By testCases = By.cssSelector("a[href='/test_cases']");
+    private By apiTesting = By.cssSelector("a[href='/api_list']");
+    private By videoTutorial = By.partialLinkText("Video Tutorials");
+    private By contactUs = By.cssSelector("a[href='/contact_us']");
 
 
     // Method
     public void clickHomePage(){
         driver.findElement(homeLink).click();
+    }
+    public void clickProducts(){
+        driver.findElement(products).click();
+    }
+    public void clickCart(){
+        driver.findElement(cart).click();
+    }
+    public void clickLogin(){
+        driver.findElement(login).click();
+    }
+    public void clickTestCases(){
+        driver.findElement(testCases).click();
+    }
+    public void clickAPITesting(){
+        driver.findElement(apiTesting).click();
+    }
+    public void clickVideoTutorial(){
+        driver.findElement(videoTutorial).click();
+    }
+    public void clickContactUs(){
+        driver.findElement(contactUs).click();
     }
 }
--- Automation-Exercise-POM-Framework/src/test/java/tests/HomePageTest.java
+++ Automation-Exercise-POM-Framework/src/test/java/tests/HomePageTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import base.BaseClass;
+import org.openqa.selenium.remote.tracing.Propagator;
 import org.testng.annotations.Test;
 import pages.HomePage;
 
@@ -12,4 +13,43 @@ public class HomePageTest extends BaseClass {
         HomePage homepage = new HomePage(driver);
         homepage.clickHomePage();
     }
+
+    @Test
+    public void verifyProducts(){
+        HomePage homePage = new HomePage(driver);
+        homePage.clickProducts();
+    }
+    @Test
+    public void verifyCart(){
+        HomePage homePage = new HomePage(driver);
+        homePage.clickCart();
+    }
+    @Test
+    public void verifyLogin(){
+        HomePage homePage = new HomePage(driver);
+        homePage.clickLogin();
+    }
+    @Test
+    public void verifyTestCases(){
+        HomePage homePage = new HomePage(driver);
+        homePage.clickTestCases();
+    }@Test
+    public void verifyAPITesting(){
+        HomePage homePage = new HomePage(driver);
+        homePage.clickAPITesting();
+    }
+    @Test
+    public void verifyVideoTutorial(){
+        HomePage homePage = new HomePage(driver);
+        homePage.clickVideoTutorial();
+    }
+    @Test
+    public void verifyContactUs(){
+        HomePage homePage = new HomePage(driver);
+        homePage.clickContactUs();
+    }
+
+
+
+
 }
